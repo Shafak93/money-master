8b43696effd4ed6e0d8b87c23ed320e66c4be56f
Shafak93 <shafakctg64@gmail.com>
2022-02-17 00:51:13 +0600

    calculate-btn function getInputValue work

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    const incomeInput = document.getElementById('income-input');
+function getInputValue(inputid){
+    const incomeInput = document.getElementById(inputid);
     const incomeInputAmount = parseInt(incomeInput.value);
+    return incomeInputAmount
+}
 
-    const foodInput = document.getElementById('food-input');
-    const foodInputAmount = parseInt(foodInput.value);
-    
-
-    const rentInput = document.getElementById('rent-input');
-    const rentInptAmount = parseInt(rentInput.value);
-    
-
-    const clothInput = document.getElementById('cloth-input');
-    const clothInputAmount = parseInt(clothInput.value);
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const incomeInputAmount = getInputValue('income-input');
+    /* const incomeInput = document.getElementById('income-input');
+    const incomeInputAmount = parseInt(incomeInput.value); */
+    const foodInputAmount = getInputValue('food-input');
+    /* const foodInput = document.getElementById('food-input');
+    const foodInputAmount = parseInt(foodInput.value); */
+    const rentInputAmount = getInputValue('rent-input');
+    /* const rentInput = document.getElementById('rent-input');
+    const rentInptAmount = parseInt(rentInput.value); */
+    const clothInputAmount = getInputValue('cloth-input');
+    /* const clothInput = document.getElementById('cloth-input');
+    const clothInputAmount = parseInt(clothInput.value); */
     
     const totalExpenses = document.getElementById('total-expenses');
     
-    totalExpenses.innerText = foodInputAmount + foodInputAmount + rentInptAmount;
+    totalExpenses.innerText = foodInputAmount + rentInputAmount + clothInputAmount;
     const totalExpensesAmount = totalExpenses.innerText;
 
     const restBalance = document.getElementById('rest-balance');
