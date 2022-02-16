2771a68ddc8dd8703f6fc7fb3bb6df86c1bcfdd8
Shafak93 <shafakctg64@gmail.com>
2022-02-17 00:39:40 +0600

    calculate-btn add event listner

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('hi')
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const incomeInput = document.getElementById('income-input');
+    const incomeInputAmount = parseInt(incomeInput.value);
+
+    const foodInput = document.getElementById('food-input');
+    const foodInputAmount = parseInt(foodInput.value);
+    
+
+    const rentInput = document.getElementById('rent-input');
+    const rentInptAmount = parseInt(rentInput.value);
+    
+
+    const clothInput = document.getElementById('cloth-input');
+    const clothInputAmount = parseInt(clothInput.value);
+    
+    const totalExpenses = document.getElementById('total-expenses');
+    
+    totalExpenses.innerText = foodInputAmount + foodInputAmount + rentInptAmount;
+    const totalExpensesAmount = totalExpenses.innerText;
+
+    const restBalance = document.getElementById('rest-balance');
+    restBalance.innerText = incomeInputAmount - totalExpensesAmount
+})
