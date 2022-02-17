d68980831b5f2edfd992904ec15087245f1fa8cc
Shafak93 <shafakctg64@gmail.com>
2022-02-17 12:10:13 +0600

    alert function

--- js/app.js
+++ js/app.js
@@ -14,41 +14,61 @@ function getInputValue(inputid){
     
 }
 
+function inputValidation(functionParameter, idParameter){
+    if(!isNaN(functionParameter) && functionParameter > 0){
+        document.getElementById(idParameter).style.display = 'none';
+        //expense alert
+        document.getElementById('expense-alert').style.display = 'none';
+        document.getElementById('total-expenses').style.display = 'block';
+    }else{
+        document.getElementById(idParameter).style.display = 'block'
+    }
+}
 document.getElementById('calculate-btn').addEventListener('click', function(){
     //Income Amount
     const incomeInputAmount = getInputValue('income-input');
-    if(!isNaN(incomeInputAmount) && incomeInputAmount > 0){
+    inputValidation(incomeInputAmount,'income-fail');
+    /* if(!isNaN(incomeInputAmount) && incomeInputAmount > 0){
         document.getElementById('income-fail').style.display = 'none'
     }else{
         document.getElementById('income-fail').style.display = 'block'
-    }
+    } */
     //Food Amount
     const foodInputAmount = getInputValue('food-input');
-    if(!isNaN(foodInputAmount) && foodInputAmount > 0){
+    inputValidation(foodInputAmount,'food-fail');
+    /* if(!isNaN(foodInputAmount) && foodInputAmount > 0){
         document.getElementById('food-fail').style.display = 'none'
     }else{
         document.getElementById('food-fail').style.display = 'block'
-    }
+    } */
     //Rent amount
     const rentInputAmount = getInputValue('rent-input');
-    if(!isNaN(rentInputAmount) && rentInputAmount > 0){
+    inputValidation(rentInputAmount,'rent-fail');
+    /* if(!isNaN(rentInputAmount) && rentInputAmount > 0){
         document.getElementById('rent-fail').style.display = 'none'
     }else{
         document.getElementById('rent-fail').style.display = 'block'
-    }
+    } */
     //Cloth Amount
     const clothInputAmount = getInputValue('cloth-input');
-    if(!isNaN(clothInputAmount) && clothInputAmount > 0){
+    inputValidation(clothInputAmount,'cloth-fail');
+    /* if(!isNaN(clothInputAmount) && clothInputAmount > 0){
         document.getElementById('cloth-fail').style.display = 'none'
     }else{
         document.getElementById('cloth-fail').style.display = 'block'
-    }
+    } */
     //Total Expenses
     const totalExpenses = document.getElementById('total-expenses');
     const totalExpensesNumber = foodInputAmount + rentInputAmount + clothInputAmount;
+    //Total expenses error validation
     if(incomeInputAmount> totalExpensesNumber){
         totalExpenses.innerText = totalExpensesNumber;
     }else{
+        
+        // totalExpenses.innerText =
+        document.getElementById('expense-alert').style.display = 'block';
+        document.getElementById('total-expenses').style.display = 'none';
+
         //alert('You do not have sufficient balance to expense.')
     }
     const totalExpensesAmount = totalExpenses.innerText;
@@ -78,7 +98,11 @@ document.getElementById('save-btn').addEventListener('click', function(){
     const savingAmountNumber = (saveInputValue / 100) * incomeInputAmount
     if(restBalance > savingAmountNumber){
         savingAmount.innerText = savingAmountNumber
+        document.getElementById('saving-amount').style.display = 'block';
+        document.getElementById('save-alert').style.display = 'none';
     }else{
+        document.getElementById('save-alert').style.display = 'block';
+        document.getElementById('saving-amount').style.display = 'none';
         //alert('You do not have sufficient balance to save')
     }
 
